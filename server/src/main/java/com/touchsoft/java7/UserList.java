a64bc100d7f04e1d79c282f4d5084a1eb9697401
Stanislau Hlushakou <stas.hlushakou@gmail.com>
2019-09-06 22:02:58 +0300

    version1

---
+++ server/src/main/java/com/touchsoft/java7/UserList.java
@@ -0,0 +1,67 @@
+package com.touchsoft.java7;
+
+
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class UserList extends Thread {
+
+    private static ArrayList<User> clientList;
+    private static ArrayList<User> agentList;
+
+
+    // Создаёт нового пользователя и добавляет его в нужную коллекцию
+    public void addUser(Socket userSocket){
+        User user = new User(userSocket);
+        if (user.getIsAgent()){
+            agentList.add(user);
+        } else {
+            clientList.add(user);
+        }
+    }
+
+    // Удаляет пользователя из нужной коллекции
+    public static void dellUser(User user){
+        if (user.getIsAgent()){
+            agentList.remove(user);
+        } else {
+            clientList.remove(user);
+        }
+    }
+
+    // Конструктор, инициализирует коллекции и запускает нить проверки пользователей, готовых к подключению
+    public UserList(){
+        clientList = new ArrayList<>();
+        agentList = new ArrayList<>();
+        start();
+    }
+
+
+    @Override
+    public void run(){
+
+        while (true) {
+
+            for (User userClient : clientList) {
+
+                if (!(userClient.getIsConnected()) && userClient.getWaitingConnection()) {
+                    for (User userAgent : agentList) {
+                        if ((!userAgent.getIsConnected()) && userAgent.getWaitingConnection()) {
+                            userClient.connectUsers(userAgent);
+                            break;
+                        }
+                    }
+                }
+            }
+
+            try {
+                sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println(e);
+            }
+
+        }
+    }
+
+
+}
